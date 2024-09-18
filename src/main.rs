e3e29ad47b0cb4e8689f5e860d9291702ee523c0
DioxusGrow <dioxusgrow@proton.me>
2024-09-18 14:06:44 +0300

    fix with dx check

--- src/main.rs
+++ src/main.rs
@@ -39,91 +39,93 @@ pub enum Route {
 fn main() {
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
     info!("starting app");
-    launch(|| {
-        use_context_provider(|| Signal::new("en".to_string()));
-        let mut lang: Signal<String> = use_context();
+    launch(App);
+}
 
-        let _ = use_resource(move || async move {
-            let mut eval = eval(
-                r#"
-                // Function to set lang and dir attributes for the <html> tag
-                function setHtmlLanguageAndDirection(lang, dir) {
-                    var htmlElement = document.documentElement;
-                    htmlElement.lang = lang;
-                    if (dir) {
-                        htmlElement.dir = dir;
-                    } else {
-                        htmlElement.removeAttribute('dir');
-                    }
-                }
+fn App() -> Element {
+    use_context_provider(|| Signal::new("en".to_string()));
+    let mut lang: Signal<String> = use_context();
 
-                let lang = localStorage.getItem("lang");
-                let browserLang = navigator.language || navigator.userLanguage;
-                let langCode = browserLang.substring(0, 2);
-
-                const supportedLangs = {
-                    "en": true,
-                    "de": true,
-                    "es": true,
-                    "ar": true
-                };
-
-                // The code checks if the browser language is in the list of supported languages,
-                // and if not, sets English as the default language.
-                if (!supportedLangs[langCode]) {
-                    langCode = "en";
-                }
-
-                if (langCode === "zh") {
-                    if (browserLang === "zh-CN" || browserLang === "zh-SG") {
-                        langCode = "zh-Hans"; // Simplified Chinese
-                    } else if (browserLang === "zh-TW" || browserLang === "zh-HK" || browserLang === "zh-MO") {
-                        langCode = "zh-Hant"; // Traditional Chinese
-                    }
-                }
-
-                // If there is no language value in local storage, set it
-                if (!lang) {
-                    localStorage.setItem("lang", langCode);
-                    lang = langCode;
-                }
-
-                // Set language and direction in HTML
-                if (lang === "ar" || lang === "he") {
-                    setHtmlLanguageAndDirection(lang, 'rtl');
+    let _ = use_resource(move || async move {
+        let mut eval = eval(
+            r#"
+            // Function to set lang and dir attributes for the <html> tag
+            function setHtmlLanguageAndDirection(lang, dir) {
+                var htmlElement = document.documentElement;
+                htmlElement.lang = lang;
+                if (dir) {
+                    htmlElement.dir = dir;
                 } else {
-                    setHtmlLanguageAndDirection(lang, null);
+                    htmlElement.removeAttribute('dir');
                 }
+            }
 
-                dioxus.send(lang);
+            let lang = localStorage.getItem("lang");
+            let browserLang = navigator.language || navigator.userLanguage;
+            let langCode = browserLang.substring(0, 2);
 
-                // How to get arr from JS to Dioxus
-                // let arr = [lang, langCode];
-                // dioxus.send(arr);
-                
-                "#,
-            );
-            let js_lang = eval.recv().await.unwrap();
-            *lang.write() = String::from(js_lang.as_str().unwrap());
+            const supportedLangs = {
+                "en": true,
+                "de": true,
+                "es": true,
+                "ar": true
+            };
 
-            // Working with array from JS
-            // if js_lang[0] == Value::Null {
-            //     // Get lang from browser lang
-            //     // *lang.write() = String::from(js_lang[1].as_str().unwrap());
-            //     // *lang.write() = String::from("de");
-            // } else {
-            //     // Get lang from browser storage
-            //     *lang.write() = String::from(js_lang[0].as_str().unwrap());
-            // }
-        });
-        info!("Lang is {}", lang());
-        rsx! {
-            head::Link { rel: "stylesheet", href: STYLE }
-            // Note: For development use only. Remove before production.
-            script { src: TAILWIND_CDN }
-            Router::<Route> {}
-        }
+            // The code checks if the browser language is in the list of supported languages,
+            // and if not, sets English as the default language.
+            if (!supportedLangs[langCode]) {
+                langCode = "en";
+            }
+
+            if (langCode === "zh") {
+                if (browserLang === "zh-CN" || browserLang === "zh-SG") {
+                    langCode = "zh-Hans"; // Simplified Chinese
+                } else if (browserLang === "zh-TW" || browserLang === "zh-HK" || browserLang === "zh-MO") {
+                    langCode = "zh-Hant"; // Traditional Chinese
+                }
+            }
+
+            // If there is no language value in local storage, set it
+            if (!lang) {
+                localStorage.setItem("lang", langCode);
+                lang = langCode;
+            }
+
+            // Set language and direction in HTML
+            if (lang === "ar" || lang === "he") {
+                setHtmlLanguageAndDirection(lang, 'rtl');
+            } else {
+                setHtmlLanguageAndDirection(lang, null);
+            }
+
+            dioxus.send(lang);
+
+            // How to get arr from JS to Dioxus
+            // let arr = [lang, langCode];
+            // dioxus.send(arr);
+            
+            "#,
+        );
+        let js_lang = eval.recv().await.unwrap();
+        *lang.write() = String::from(js_lang.as_str().unwrap());
+
+        // Working with array from JS
+        // if js_lang[0] == Value::Null {
+        //     // Get lang from browser lang
+        //     // *lang.write() = String::from(js_lang[1].as_str().unwrap());
+        //     // *lang.write() = String::from("de");
+        // } else {
+        //     // Get lang from browser storage
+        //     *lang.write() = String::from(js_lang[0].as_str().unwrap());
+        // }
     });
+    info!("Lang is {}", lang());
+    rsx! {
+        head::Link { rel: "stylesheet", href: STYLE }
+        // Note: For development use only. Remove before production.
+        script { src: TAILWIND_CDN }
+        Router::<Route> {}
+    }
 }
 
 #[component]
@@ -188,7 +190,7 @@ fn NavBar() -> Element {
                                 Link {
                                     onclick: move |_| {
                                         lang.set(code.to_string());
-
+                        
                                         let eval = eval(r#"
                                         // Function to set lang and dir attributes for the <html> tag
                                         function setHtmlLanguageAndDirection(lang, dir) {
@@ -212,7 +214,7 @@ fn NavBar() -> Element {
                                         }
                                         
                                         "#);
-
+                        
                                         eval.send(code.into()).unwrap();
                                     },
                                     to: Route::Home {},
@@ -223,7 +225,7 @@ fn NavBar() -> Element {
                                 Link {
                                     onclick: move |_| {
                                         lang.set(code.to_string());
-
+                        
                                         let eval = eval(r#"
                                         // Function to set lang and dir attributes for the <html> tag
                                         function setHtmlLanguageAndDirection(lang, dir) {
@@ -247,7 +249,7 @@ fn NavBar() -> Element {
                                         }
                                         
                                         "#);
-
+                        
                                         eval.send(code.into()).unwrap();
                                     },
                                     to: Route::HomeLang {
